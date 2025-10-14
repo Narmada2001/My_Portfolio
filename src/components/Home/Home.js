e206c2a58a8c2ea4a1853a730e420cc1c3c1babd
Narmada2001 <155349750+Narmada2001@users.noreply.github.com>
2025-10-15 01:31:56 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,6 +19,7 @@ function Home() {
             width: "100%",
             height: "100%",
             zIndex: 0,
+            pointerEvents: "none"
           }}
         >
           <Particle />
